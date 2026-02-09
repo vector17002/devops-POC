22efe95668dc6c533f74914646a8bcf931dea1be
vector17002 <vector17002>
2026-02-09 23:35:45 +0530

    added logging and middleware

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,20 @@
 import express from "express";
+import logger from "#config/logger.js";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
 
+app.use(helmet());
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+app.use(morgan('combined', { stream: { write: (message: string) => logger.info(message) } }));
+
 app.get('/', (req, res) => {
+    logger.info('Hello from this POC backend')
     res.status(200).send(`Hello from ${process.env.APP_NAME} api`);
 })
 
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import app from "./app.js";
+import logger from "#config/logger.js";
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+    logger.info(`Server started on port ${PORT}`);
 });
