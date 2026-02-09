978db26d0c2936af7ba86c9931cd6fa499d3890a
vector17002 <vector17002>
2026-02-09 22:35:54 +0530

    database integrated

---
+++ src/models/user.model.ts
@@ -0,0 +1,13 @@
+import { pgTable, serial, text, timestamp, pgEnum } from "drizzle-orm/pg-core";
+
+export const userRoleEnum = pgEnum("user_role", ["user", "admin"]);
+
+export const users = pgTable("users", {
+    id: serial("id").primaryKey(),
+    name: text("name").notNull(),
+    email: text("email").notNull().unique(),
+    password: text("password").notNull(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at").notNull().defaultNow().$onUpdate(),
+    role: userRoleEnum("role").notNull().default("user"),
+});
